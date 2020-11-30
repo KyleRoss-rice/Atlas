c2d54c3159aa4bbe47d9b7b0200aae4d1e34767f
Kyle Ross <62770403+KyleRoss-rice@users.noreply.github.com>
2020-11-30 18:47:47 -0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -104,7 +104,7 @@ def process(info_file, model, num_frames, save_path, total_scenes_index, total_s
         mesh_pred = tsdf_pred.get_mesh()
 
     tsdf_pred.save(os.path.join(save_path, '%s.npz'%scene))
-    mesh_pred.export(os.path.join(save_path, '%s.obj'%scene))
+    mesh_pred.export(os.path.join(save_path, '%s.ply'%scene))
 
 
 
